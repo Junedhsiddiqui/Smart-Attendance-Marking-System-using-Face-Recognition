8da7e82ee9fc1cfbb0a005b0a5c74815711e917e
Junedh Siddiqui <91174575+Junedhsiddiqui@users.noreply.github.com>
2023-07-12 21:31:31 +0530

    Update attendance.py

--- attendance_system-master/attendance.py
+++ attendance_system-master/attendance.py
@@ -15,7 +15,7 @@ window = tk.Tk()
 window.title("Attendance System")
 
 
-window.configure(background='pink')
+window.configure(background='light blue')
 
 
 window.grid_rowconfigure(0, weight=1)
@@ -25,14 +25,14 @@ x_cord = 75;
 y_cord = 20;
 checker=0;
 
-message = tk.Label(window, text="GEHU UNIVERSITY" ,bg="white"  ,fg="black"  ,width=20  ,height=2,font=('Times New Roman', 25, 'bold')) 
-message.place(x=1150, y=760)
+message = tk.Label(window, text="GEHU UNIVERSITY" ,bg="black"  ,fg="red"  ,width=20  ,height=2,font=('Times New Roman', 25, 'bold underline')) 
+message.place(x=550, y=80)
 
-message = tk.Label(window, text="ATTENDANCE MANAGEMENT PORTAL" ,bg="pink"  ,fg="black"  ,width=40  ,height=1,font=('Times New Roman', 35, 'bold underline')) 
+message = tk.Label(window, text="ATTENDANCE MANAGEMENT PORTAL" ,bg="red"  ,fg="white"  ,width=40  ,height=1,font=('Times New Roman', 35, 'bold underline')) 
 message.place(x=200, y=20)
 
-lbl = tk.Label(window, text="Enter Your College ID",width=20  ,height=2  ,fg="black"  ,bg="Pink" ,font=('Times New Roman', 25, ' bold ') ) 
-lbl.place(x=200-x_cord, y=200-y_cord)
+lbl = tk.Label(window, text="Enter College ID",width=20  ,height=2  ,fg="black"  ,bg="Pink" ,font=('Times New Roman', 25, ' bold ') ) 
+lbl.place(x=180-x_cord, y=200-y_cord)
 
 
 txt = tk.Entry(window,width=30,bg="white" ,fg="blue",font=('Times New Roman', 15, ' bold '))
@@ -44,26 +44,26 @@ lbl2.place(x=600-x_cord, y=200-y_cord)
 txt2 = tk.Entry(window,width=30  ,bg="white"  ,fg="blue",font=('Times New Roman', 15, ' bold ')  )
 txt2.place(x=650-x_cord, y=300-y_cord)
 
-lbl3 = tk.Label(window, text="NOTIFICATION",width=20  ,fg="black"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 25, ' bold ')) 
+lbl3 = tk.Label(window, text="Notification Screen",width=20  ,fg="black"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 25, ' bold ')) 
 lbl3.place(x=1060-x_cord, y=200-y_cord)
 
 message = tk.Label(window, text="" ,bg="white"  ,fg="blue"  ,width=30  ,height=1, activebackground = "white" ,font=('Times New Roman', 15, ' bold ')) 
 message.place(x=1075-x_cord, y=300-y_cord)
 
-lbl3 = tk.Label(window, text="ATTENDANCE",width=20  ,fg="white"  ,bg="lightgreen"  ,height=2 ,font=('Times New Roman', 30, ' bold ')) 
+lbl3 = tk.Label(window, text="ATTENDANCE",width=20  ,fg="white"  ,bg="black"  ,height=2 ,font=('Times New Roman', 30, ' bold ')) 
 lbl3.place(x=120, y=570-y_cord)
 
 
-message2 = tk.Label(window, text="" ,fg="red"   ,bg="yellow",activeforeground = "green",width=60  ,height=4  ,font=('times', 15, ' bold ')) 
+message2 = tk.Label(window, text="" ,fg="red"   ,bg="white",activeforeground = "green",width=60  ,height=4  ,font=('times', 15, ' bold ')) 
 message2.place(x=700, y=570-y_cord)
 
-lbl4 = tk.Label(window, text="STEP 1",width=20  ,fg="green"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 20, ' bold '))
+lbl4 = tk.Label(window, text="STEP 1",width=20  ,fg="black"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 20, ' bold '))
 lbl4.place(x=240-x_cord, y=375-y_cord)
 
-lbl5 = tk.Label(window, text="STEP 2",width=20  ,fg="green"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 20, ' bold ')) 
+lbl5 = tk.Label(window, text="STEP 2",width=20  ,fg="black"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 20, ' bold ')) 
 lbl5.place(x=645-x_cord, y=375-y_cord)
 
-lbl6 = tk.Label(window, text="STEP 3",width=20  ,fg="green"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 20, ' bold ')) 
+lbl6 = tk.Label(window, text="STEP 3",width=20  ,fg="black"  ,bg="pink"  ,height=2 ,font=('Times New Roman', 20, ' bold ')) 
 lbl6.place(x=1100-x_cord, y=362-y_cord)
  
 def clear1():
@@ -109,20 +109,29 @@ def TakeImages():
             tk.messagebox.showinfo('Your need','Please go through the readme file properly')
         
     elif(is_number(Id) and name.isalpha()):
+            print("db")
             cam = cv2.VideoCapture(0)
-            harcascadePath = "haarcascade_frontalface_default.xml"
+            print("jfb")
+            harcascadePath = "./haarcascade_frontalface_default.xml"
             detector=cv2.CascadeClassifier(harcascadePath)
             sampleNum=0
             while(True):
                 ret, img = cam.read()
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                print(gray)
                 faces = detector.detectMultiScale(gray, 1.3, 5)
+                print(faces)
+                cv2.imshow('frame',img)
                 for (x,y,w,h) in faces:
                     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)        
                     #incrementing sample number 
                     sampleNum=sampleNum+1
                     #saving the captured face in the dataset folder TrainingImage
-                    cv2.imwrite("TrainingImage\ "+name +"."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+                    val =   cv2.imwrite("TrainingImage\ "+name +"."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+                    if val == True:
+                        print("saved image")
+                    else:
+                        print("not saved")
                     #display the frame
                     cv2.imshow('frame',img)
                 #wait for 100 miliseconds 
@@ -131,6 +140,7 @@ def TakeImages():
                 # break if the sample number is morethan 100
                 elif sampleNum>60:
                     break
+            print("hjh")
             cam.release()
             cv2.destroyAllWindows() 
             res = "Images Saved for ID : " + Id +" Name : "+ name
@@ -190,7 +200,8 @@ def TrackImages():
     cam = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX        
     col_names =  ['Id','Name','Date','Time']
-    attendance = pd.DataFrame(columns = col_names)    
+    attendance = pd.DataFrame(columns = col_names)
+    isIdentified = False    
     while True:
         ret, im =cam.read()
         gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -205,19 +216,15 @@ def TrackImages():
                 aa=df.loc[df['Id'] == Id]['Name'].values
                 tt=str(Id)+"-"+aa
                 attendance.loc[len(attendance)] = [Id,aa,date,timeStamp]
-                
+                isIdentified  = True
             else:
                 Id='Unknown'                
                 tt=str(Id)  
-            if(conf > 75):
-                noOfFile=len(os.listdir("ImagesUnknown"))+1
-                cv2.imwrite("ImagesUnknown\Image"+str(noOfFile) + ".jpg", im[y:y+h,x:x+w])            
             cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)        
         attendance=attendance.drop_duplicates(subset=['Id'],keep='first')    
-        print("hello 1")
         cv2.imshow('im',im) 
-        #(cv2.waitKey(1)==ord('q')):
-        break
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            break
     ts = time.time()      
     date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
     timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
@@ -230,19 +237,21 @@ def TrackImages():
     message2.configure(text= res)
     res = "Attendance Taken"
     message.configure(text= res)
-    tk.messagebox.showinfo('Completed','Congratulations ! Your attendance has been marked successfully for the day!!')
-    
+    if isIdentified == True:
+        tk.messagebox.showinfo('Completed','Congratulations ! Your attendance has been marked successfully for the day!!')
+    else:
+        tk.messagebox.showinfo('Failed','Unable to recognize face!')
 def quit_window():
    MsgBox = tk.messagebox.askquestion ('Exit Application','Are you sure you want to exit the application',icon = 'warning')
    if MsgBox == 'yes':
        tk.messagebox.showinfo("Greetings", "Thank You very much for using our software. Have a nice day ahead!!")
        window.destroy()
     
-takeImg = tk.Button(window, text="IMAGE CAPTURE BUTTON", command=TakeImages  ,fg="white"  ,bg="blue"  ,width=25  ,height=2, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
+takeImg = tk.Button(window, text="IMAGE CAPTURE BUTTON", command=TakeImages  ,fg="white"  ,bg="green"  ,width=25  ,height=2, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
 takeImg.place(x=245-x_cord, y=425-y_cord)
-trainImg = tk.Button(window, text="MODEL TRAINING BUTTON", command=TrainImages  ,fg="white"  ,bg="blue"  ,width=25  ,height=2, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
+trainImg = tk.Button(window, text="MODEL TRAINING BUTTON", command=TrainImages  ,fg="white"  ,bg="green"  ,width=25  ,height=2, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
 trainImg.place(x=645-x_cord, y=425-y_cord)
-trackImg = tk.Button(window, text="ATTENDANCE MARKING BUTTON", command=TrackImages  ,fg="white"  ,bg="red"  ,width=30  ,height=3, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
+trackImg = tk.Button(window, text="ATTENDANCE MARKING BUTTON", command=TrackImages  ,fg="white"  ,bg="green"  ,width=30  ,height=3, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
 trackImg.place(x=1075-x_cord, y=412-y_cord)
 quitWindow = tk.Button(window, text="QUIT", command=quit_window  ,fg="white"  ,bg="red"  ,width=10  ,height=2, activebackground = "pink" ,font=('Times New Roman', 15, ' bold '))
 quitWindow.place(x=700, y=735-y_cord)
